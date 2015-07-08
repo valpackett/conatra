ccda4d89411e9921656641d691b3076a3a1dfae3
Greg V <greg@unrelenting.technology>
2015-07-08 21:57:18 +0300

    update weather sketch

--- examples/iotweather/iotweather.ino
+++ examples/iotweather/iotweather.ino
@@ -16,16 +16,20 @@ static uint8_t mymac[] = { 0x74, 0x69, 0x69, 0x2D, 0x30, 0x31 };
 
 //// Sensor Config ////
 
-#define FLAME_PIN             0
-#define FLAME_ALERT_THRESHOLD 190
-#define LIGHT_PIN             1
+#define FLAME_PIN             0     // analog
+#define FLAME_ALERT_THRESHOLD 100
+
+#define MQ2_PIN               2     // analog
+#define MQ2_ALERT_THRESHOLD   115
+
+#define PIR_PIN               8     // digital
 
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085_U.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define DHT_PIN  0
+#define DHT_PIN  0 // digital
 #define DHT_TYPE DHT11
 DHT_Unified dht(DHT_PIN, DHT_TYPE);
 
@@ -36,34 +40,33 @@ Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
 #define BUZZER_PIN  5
 #define BUZZER_HIGH 30
 
-//// LCD Config ////
-
-#include <PCD8544_SPI.h>
-
-PCD8544_SPI lcd;
-
 //// Data Memory ////
 
-static uint8_t temp;
-static uint8_t humidity;
-static uint8_t flame;
-static uint16_t light;
-static float pressure;
+static uint8_t  temp;
+static uint8_t  humidity;
+static uint8_t  flame;
+static uint8_t  mq2_gas;
+static bool     motion;
+static float    pressure;
+
+static uint32_t timer;
+static uint32_t last_alert_time;
 
 //// Setup & Loop ////
 
 void setup(void) {
-  lcd.begin(false, 0xBD, 0x04, 0x13);
+  last_alert_time = millis() + 20 * 1000; // Wait for the MQ2 to stabilize, do not beep
   bmp.begin();
   dht.begin();
   if (ether.begin(sizeof Ethernet::buffer, mymac, 4) == 0)
-    Serial.println(F("Eth"));
+    Serial.println(F("EthErr"));
   if (!ether.dhcpSetup())
-    Serial.println(F("DHCP"));
+    Serial.println(F("DHCPErr"));
+  else
+    ether.printIp("My IP: ", ether.myip);
   coap_ethercard_begin();
+  coap_ethercard_begin_multicast();
 }
-
-static uint32_t timer;
 
 void loop(void) {
   timer = millis();
@@ -76,39 +79,24 @@ void loop(void) {
   }
   if (timer % 2000 == 0) {
     readDigitalSensors();
-    printInfoOnScreen();
   }
   delay(50);
 }
 
-void printInfoOnScreen() {
-  lcd.gotoXY(0, 0);
-  lcd.print(temp);
-  lcd.print(F(" C "));
-  lcd.print(humidity);
-  lcd.print(F(" % "));
-  lcd.print(F("    "));
-  lcd.print(pressure);
-  lcd.print(F(" mmHg   "));
-  for (uint8_t i = 0; i < 4; ++i) {
-    lcd.print(ether.myip[i], DEC);
-    if (i < 3)
-      lcd.print(F("."));
-  }
-}
-
 void readDigitalSensors() {
+  motion = digitalRead(PIR_PIN) == HIGH;
+  
   sensors_event_t event;
 
   dht.temperature().getEvent(&event);
   temp = event.temperature;
   if (isnan(event.temperature))
-    lcd.print(F("Err"));
+    Serial.println(F("TempErr"));
 
   dht.humidity().getEvent(&event);
   humidity = event.relative_humidity;
   if (isnan(event.relative_humidity))
-    lcd.print(F("Err"));
+    Serial.println(F("HumidErr"));
 
   bmp.getEvent(&event);
   pressure = event.pressure * 0.75; // hPa -> mmHg
@@ -120,13 +108,16 @@ void readDigitalSensors() {
 
 void readAnalogSensors() {
   flame = analogRead(FLAME_PIN);
-  light = analogRead(LIGHT_PIN);
+  mq2_gas = analogRead(MQ2_PIN);
 }
 
 void writeAnalogOutputs() {
   analogWrite(BUZZER_PIN, 0);
-  if (flame > FLAME_ALERT_THRESHOLD)
+  if ((flame > FLAME_ALERT_THRESHOLD || mq2_gas > MQ2_ALERT_THRESHOLD)
+      && millis() - last_alert_time > 1000) {
     beep();
+    last_alert_time = millis();
+  }
 }
 
 void beep() {
@@ -155,8 +146,12 @@ ROUTE(flame, COAP_METHOD_GET, URL("flame"), ";if=\"sensor\"", { \
   char rspc[3]; itoa(flame, rspc, 10); \
   CONTENT(COAP_CONTENTTYPE_TEXT_PLAIN, rspc); \
 }) \
-ROUTE(light, COAP_METHOD_GET, URL("light"), ";if=\"sensor\"", { \
-  char rspc[5]; itoa(light, rspc, 10); \
+ROUTE(gas, COAP_METHOD_GET, URL("gas"), ";if=\"sensor\"", { \
+  char rspc[5]; itoa(mq2_gas, rspc, 10); \
+  CONTENT(COAP_CONTENTTYPE_TEXT_PLAIN, rspc); \
+}) \
+ROUTE(motion, COAP_METHOD_GET, URL("motion"), ";if=\"sensor\"", { \
+  char rspc[2];  itoa(motion, rspc, 10); \
   CONTENT(COAP_CONTENTTYPE_TEXT_PLAIN, rspc); \
 }) \
 ROUTE(buzzer, COAP_METHOD_POST, URL("buzzer"), ";if=\"alarm\"", { \
